ae60f3452fed7f2a5f65d6f333deb82cf46e4af3
Jack Crane <jack@jackcrane.rocks>
2026-02-26 17:59:03 -0600

    Individual homing

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,45 +1,40 @@
-// limits=7,6
+#include <AccelStepper.h>
 
 const int stepPin1 = 2;
 const int dirPin1  = 3;
-
 const int stepPin2 = 4;
 const int dirPin2  = 5;
 
-unsigned long lastStepTime1 = 0;
-unsigned long lastStepTime2 = 0;
+const int limitPin1 = 7;
+const int limitPin2 = 6;
 
-const unsigned long stepInterval1 = 50;  // microseconds (speed motor 1)
-const unsigned long stepInterval2 = 50; // microseconds (speed motor 2)
+AccelStepper stepper1(AccelStepper::DRIVER, stepPin1, dirPin1);
+AccelStepper stepper2(AccelStepper::DRIVER, stepPin2, dirPin2);
 
-bool stepState1 = HIGH;
-bool stepState2 = HIGH;
+void homeOne(AccelStepper &stepper, int limitPin) {
+  stepper.setMaxSpeed(800);
+  stepper.setAcceleration(400);
+
+  stepper.moveTo(-100000);  // move toward switch
+
+  while (digitalRead(limitPin) == 0) {  // move UNTIL switch == 1
+    stepper.run();
+  }
+
+  stepper.stop();
+  stepper.setCurrentPosition(0);
+}
 
 void setup() {
-  pinMode(stepPin1, OUTPUT);
-  pinMode(dirPin1, OUTPUT);
+  pinMode(limitPin1, INPUT);
+  pinMode(limitPin2, INPUT);
 
-  pinMode(stepPin2, OUTPUT);
-  pinMode(dirPin2, OUTPUT);
+  Serial.begin(9600);
 
-  digitalWrite(dirPin1, HIGH); // direction motor 1
-  digitalWrite(dirPin2, HIGH);  // direction motor 2
+  homeOne(stepper1, limitPin1);
+  homeOne(stepper2, limitPin2);
+
+  Serial.println("Homed");
 }
 
-void loop() {
-  unsigned long currentMicros = micros();
-
-  // Motor 1
-  if (currentMicros - lastStepTime1 >= stepInterval1) {
-    lastStepTime1 = currentMicros;
-    stepState1 = !stepState1;
-    digitalWrite(stepPin1, stepState1);
-  }
-
-  // Motor 2
-  if (currentMicros - lastStepTime2 >= stepInterval2) {
-    lastStepTime2 = currentMicros;
-    stepState2 = !stepState2;
-    digitalWrite(stepPin2, stepState2);
-  }
-}
+void loop() {}
