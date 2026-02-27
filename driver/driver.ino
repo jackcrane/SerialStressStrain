7ba11c6c5e26af87ca17518cc27dfc6128524b76
Jack Crane <jack@jackcrane.rocks>
2026-02-26 18:23:32 -0600

    fast homing

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,5 +1,3 @@
-#include <AccelStepper.h>
-
 const int stepPin1 = 2;
 const int dirPin1  = 3;
 const int stepPin2 = 4;
@@ -8,50 +6,56 @@ const int dirPin2  = 5;
 const int limitPin1 = 7;
 const int limitPin2 = 6;
 
-AccelStepper stepper1(AccelStepper::DRIVER, stepPin1, dirPin1);
-AccelStepper stepper2(AccelStepper::DRIVER, stepPin2, dirPin2);
+// Smaller = faster
+const int stepDelayMicros = 25;   // try 100, 75, 50 if your driver allows
 
 void homeBoth() {
 
-  stepper1.setMaxSpeed(10000);
-  stepper1.setAcceleration(400);
-
-  stepper2.setMaxSpeed(10000);
-  stepper2.setAcceleration(400);
-
-  // Move toward switches (flip sign if wrong direction)
-  stepper1.moveTo(-100000);
-  stepper2.moveTo(-100000);
+  digitalWrite(dirPin1, LOW);   // homing direction
+  digitalWrite(dirPin2, LOW);
 
   bool homed1 = false;
   bool homed2 = false;
 
   while (!homed1 || !homed2) {
+
+    if (!homed1 && digitalRead(limitPin1) == HIGH) {
+      homed1 = true;
+    }
+
+    if (!homed2 && digitalRead(limitPin2) == HIGH) {
+      homed2 = true;
+    }
+
+    // Step motor 1
     if (!homed1) {
-      if (digitalRead(limitPin1) == 1) {
-        stepper1.stop();
-        stepper1.setCurrentPosition(0);
-        homed1 = true;
-      } else {
-        stepper1.run();
-      }
+      digitalWrite(stepPin1, HIGH);
+    }
+
+    // Step motor 2
+    if (!homed2) {
+      digitalWrite(stepPin2, HIGH);
+    }
+
+    delayMicroseconds(stepDelayMicros);
+
+    if (!homed1) {
+      digitalWrite(stepPin1, LOW);
     }
 
     if (!homed2) {
-      if (digitalRead(limitPin2) == 1) {
-        stepper2.stop();
-        stepper2.setCurrentPosition(0);
-        homed2 = true;
-      } else {
-        stepper2.run();
-      }
+      digitalWrite(stepPin2, LOW);
     }
+
+    delayMicroseconds(stepDelayMicros);
   }
 }
 
 void setup() {
-  pinMode(limitPin1, INPUT);
-  pinMode(limitPin2, INPUT);
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
 
   pinMode(limitPin1, INPUT_PULLUP);
   pinMode(limitPin2, INPUT_PULLUP);
